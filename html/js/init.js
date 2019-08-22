07358e4839fa36cf628b220e7ac4f5820c14b398
Joe Dei Rossi <deirossijoseph@gmail.com>
2019-08-22 13:52:51 -0700

    Removed image effects, updated changing text on hero section

--- html/js/init.js
+++ html/js/init.js
@@ -540,7 +540,7 @@ function arlo_tm_animate_text(){
 	var animateSpan			= jQuery('.arlo_tm_animation_text_word');
 	
 		animateSpan.typed({
-			strings: ["Freelancer", "UI/UX Designer", "Web Developer"],
+			strings: ["Web Developer", "Software Developer"],
 			loop: true,
 			startDelay: 1e3,
 			backDelay: 2e3
